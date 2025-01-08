f729aa56bded8c03bf1865abf6f14d10df9f2497
unknown <twfarro@wpi.edu>
2025-01-07 21:13:00 -0500

    punctuation pauses!

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const WebSocket = require('ws');
-const app = express();
+const { app, BrowserWindow } = require('electron');
+const expressServer = express();
 require('dotenv').config();
 
 const baseDirectory = path.join(__dirname, './public');
@@ -57,36 +58,49 @@ fs.watch(wordsDirectory, async e => {
 });
 
 /**
+ * @typedef {Command}
+ * @property {string} path
+ * @property {number} delay
+ */
+
+/**
  * 
  * @param {string} phrase 
  * @param {Map<string, string[]>} dictionary 
+ * @return {Command[]} commands
  */
 function formSentence(phrase, dictionary){
     const segmenter = new Intl.Segmenter('en', { granularity: 'word' });
     const split = [...segmenter.segment(phrase.toLowerCase())]
         .filter((e) => e.segment.trim() !== "");
     console.log(split);
-    const files = []
+    const commands = []
     for(const word of split){
         if(word.isWordLike){
             if(dictionary.has(word.segment)){
-                files.push(dictionary.get(word.segment)[Math.floor(Math.random() * dictionary.get(word.segment).length)])
+                commands.push({
+                    path: dictionary.get(word.segment)[Math.floor(Math.random() * dictionary.get(word.segment).length)]
+                })
             }
         }else{
             // punctuation, push a pause!
+            commands.push({
+                delay: 500,
+            })
         }
     }
-    return files;
+    return commands;
 }
 
 async function launch() {
     wordsDictionary = await parseDictionary();
     const port = process.env.PORT || 8095;
-    app.use('/', express.static(baseDirectory));
-    app.set('trust proxy', true);
+    expressServer.use('/', express.static(baseDirectory));
+    expressServer.set('trust proxy', true);
+    expressServer.use(express.json());
 
     // Makes an http server out of the express server
-    const httpServer = http.createServer(app);
+    const httpServer = http.createServer(expressServer);
 
     // Starts the http server
     httpServer.listen(port, () => {
@@ -94,25 +108,30 @@ async function launch() {
         console.log(`started on ${port}`);
     });
 
-    const server = new WebSocket.Server({server: httpServer, path:'/'});
-    server.on('connection', (ws) => {
+    const wsServer = new WebSocket.Server({server: httpServer, path:'/'});
+    expressServer.on('connection', (ws) => {
         console.log('connection!');
     });
 
-    server.on('close', (ws) => {
+    expressServer.on('close', (ws) => {
         console.log('connection closed!');
     });
 
-    app.get(['/source', '/player',], async (req, res) => {
+    expressServer.post(['/delay'], async (req, res) => {
+        console.log(req.body);
+        res.sendStatus(200);
+    })
+
+    expressServer.get(['/source', '/player',], async (req, res) => {
         res.status(200).sendFile(path.join(baseDirectory, 'index.html'));
         return;
     });
 
-    app.get(['/speak',], async (req, res) => {
+    expressServer.get(['/speak',], async (req, res) => {
         if(req.query.phrase && req.query){
             console.log(req.query.phrase);
             const files = formSentence(req.query.phrase, wordsDictionary);
-            server.clients.forEach(client => {
+            wsServer.clients.forEach(client => {
                 client.send(
                     JSON.stringify({
                         files
@@ -126,10 +145,29 @@ async function launch() {
         return;
     });
 
-    app.get(['/words',], async (req, res) => {
+    expressServer.get(['/words',], async (req, res) => {
         console.log(wordsDictionary);
         res.status(200).send([...wordsDictionary.keys()]);
         return;
+    });
+
+    const createWindow = () => {
+        const win = new BrowserWindow({
+          width: 800,
+          height: 600
+        })
+      
+        win.loadFile('home.html');
+    }
+
+    app.whenReady().then(() => {
+        createWindow()
+    });
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit();   
+        }
     });
 }
 
